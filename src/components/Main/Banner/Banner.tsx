e4718bddb3670fe8d7cf18abb52b3165a2079ff5
twk5989 <a37225989@gmail.com>
2025-08-25 18:11:33 +0900

    [#48] Chore: 코드 분리 및 공용 유틸 추가

--- src/components/Main/Banner/Banner.tsx
+++ src/components/Main/Banner/Banner.tsx
@@ -2,20 +2,33 @@ import React from 'react';
 import * as S from './Banner.styles';
 import arrow_right_circle from '../../../assets/icons/arrow_right_circle.svg';
 import bell from '../../../assets/icons/notifications_banner.svg';
-import { BannerProps } from './Banner.types';
+import { useNavigate } from 'react-router-dom';
+import type { BannerProps } from './Banner.types';
 
-// TODO Body값 받아와서 뿌려주기(type), 바로가기 네비게이트 설정
+export default function Banner({
+  title = '저장된 공고 중에 \n기간이 끝나가는 것이 있어요!',
+  body = '국내 정기거주 아동 교육권 보장을 위한 체류자격 부여 방안',
+}: BannerProps) {
+  const navigate = useNavigate();
+  const goBookmarked = () => navigate('/bookmarked');
 
-export default function Banner() {
   return (
     <S.BannerContainer aria-label="채용 공고 알림 배너">
       <S.Title>
-        저장된 공고 중에 <br /> 기간이 끝나가는 것이 있어요!
+        {title.split('\n').map((line, i) => (
+          <React.Fragment key={i}>
+            {line}
+            {i === 0 && <br />}
+          </React.Fragment>
+        ))}
       </S.Title>
-      <S.Body>국내 정기거주 아동 교육권 보장을 위한 체류자격 부여 방안</S.Body>
-      <S.Button>
+
+      <S.Body>{body}</S.Body>
+
+      <S.Button type="button" onClick={goBookmarked} aria-label="북마크한 공고 페이지로 이동">
         바로가기 <img src={arrow_right_circle} alt="" />
       </S.Button>
+
       <S.BellIcon>
         <img src={bell} alt="알림 벨 아이콘" />
       </S.BellIcon>
